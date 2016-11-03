057ab639200711372f7ef29b38a04fd94eda304f
Dheeraj Mummareddy <Dheeraj.Mummareddy@nike.com>
2016-11-02 20:57:49 -0700

    add groovy tests

---
+++ src/test/groovy/HelloWorldAppSpec.groovy
@@ -0,0 +1,23 @@
+import spock.lang.Specification
+
+/**
+ * Created by Dheeraj Mummareddy on 11/2/16.
+ */
+class HelloWorldAppSpec extends Specification {
+    def setupSpec() {
+    }
+
+    def cleanupSpec() {
+    }
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    def "canary"() {
+        expect:
+        true
+    }
+}
